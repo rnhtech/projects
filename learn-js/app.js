81f4299de263109361ffd349026d2ebcb34c10e8
RNH TECH <rnhtech@riverdaleonline.org>
2016-06-16 17:19:32 -0400

    Added js-learning project

---
+++ learn-js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.scrollspy').scrollSpy();
+
+
+	//Exercises buttons handllers 
+	$('#operators-exe-btn').click(function(){
+		$('.operators-iframe').removeClass('hidden');
+	});
+});			
